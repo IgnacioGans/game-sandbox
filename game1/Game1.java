d2234e13d4aceee703d6b155e9eefaa9e3807493
HandyAndy136 <langy26@gmail.com>
2014-11-25 16:02:16 +1030

    Game1 written - basic panel

---
+++ game1/Game1.java
@@ -0,0 +1,11 @@
+// package com.HandyAndy136.minitennis1;
+import javax.swing.JFrame;
+
+public class Game {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("mini tennis");
+		frame.setSize(300,300);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
