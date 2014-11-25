e0de6720a2959234c902ff6658fa198d8eab9043
HandyAndy136 <langy26@gmail.com>
2014-11-25 16:37:12 +1030

    A ball moving across the screen. Brilliant, I tell you

---
+++ game2/Game.java
@@ -0,0 +1,39 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Game extends JPanel {
+
+	int mX = 0;
+	int mY = 0;
+
+	private void moveBall() {
+		mX++;
+		mY++;
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g2d.fillOval(mX,mY,30,30);
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		JFrame frame = new JFrame("Mini Tennis");
+		Game game = new Game();
+		frame.add(game);
+		frame.setSize(300,400);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		while (true) {
+			game.moveBall();
+			game.repaint();
+			Thread.sleep(10);
+		}
+	}
+}
