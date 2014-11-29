535a5cda8418e70a5e1e003883d06de69c2695d9
HandyAndy136 <langy26@gmail.com>
2014-11-29 15:34:49 +1030

    Donut written, further directory organisation

---
+++ zetcode/01-basics/01-donut/Donut.java
@@ -0,0 +1,26 @@
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Donut extends JFrame {
+	public Donut() {
+		initUI();
+	}
+
+	private void initUI() {
+		add(new Board());
+		setSize(330,330);
+		setTitle("Donut");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+	}
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				Donut dn = new Donut();
+				dn.setVisible(true);
+			}
+		});
+	}
+}
