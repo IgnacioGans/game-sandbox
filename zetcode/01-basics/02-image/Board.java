66e63aa6d070a84b64fd9ec3b474abca2499c329
HandyAndy136 <langy26@gmail.com>
2014-11-29 15:41:45 +1030

    Drawing images! Genius

---
+++ zetcode/01-basics/02-image/Board.java
@@ -0,0 +1,30 @@
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class Board extends JPanel {
+	private Image mImg;
+
+	public Board() {
+		initBoard();
+	}
+
+	private void initBoard() {
+		loadImage();
+		int w = mImg.getWidth();
+		int h = mImg.getHeight();
+		setPreferredSize(new Dimension(w,h));
+	}
+
+	private void loadImage() {
+		ImageIcon ii = new ImageIcon("derp.jpg");
+		mImg = ii.getImage();
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		g.drawImage(mImg, 0, 0, null);
+	}
+}
